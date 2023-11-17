3d13a4736894170a2aa877f0b2754f0b01f42a20
jeison.alarcon <alarcon_osorio@hotmail.com>
2023-11-17 15:10:01 -0500

    Fix Index

--- index.php
+++ index.php
@@ -4,22 +4,31 @@ include 'includes/conect.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Verifica el inicio de sesión
-$sql = "SELECT * FROM usuarios WHERE username='$username' AND password='$password'";
-$result = $conn->query($sql);
-
-if ($result->num_rows == 1) {
-    $_SESSION['username'] = $username;
-    header("Location: welcome.php"); // Redirige al usuario a una página de bienvenida
-} else {
+if($username == null || $password == null){
     echo "<script>
-            alert('Nombre de usuario o contraseña incorrectos.');
 			setTimeout(function(){
                 window.location.href = 'login.php';
             }, 0000);
           </script>";
+}else{
+    $sql = "SELECT * FROM usuarios WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $_SESSION['username'] = $username;
+        header("Location: welcome.php"); // Redirige al usuario a una página de bienvenida
+    } else {
+        echo "<script>
+                alert('Nombre de usuario o contraseña incorrectos.');
+                setTimeout(function(){
+                    window.location.href = 'login.php';
+                }, 0000);
+            </script>";
+    }   
 }
+
+// Verifica el inicio de sesión
+
 
 $conn->close();
 ?>
-
